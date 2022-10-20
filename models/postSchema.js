2b5a5db26e7fd7b546fe71c39da9660697bf4b87
bananabread08 <rmkfermin@gmail.com>
2022-10-20 11:06:33 +0800

    create Post and User Schemas

---
+++ models/postSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  title: { type: String, required: true },
+  message: { type: String, required: true },
+  date_posted: { type: Date, default: Date.now },
+});
+
+PostSchema.virtual('url').get(function () {
+  return `/post/${this._id}`;
+});
+
+PostSchema.virtual('date_diff').get(function () {
+  const rtf = new Intl.RelativeTimeFormat('en', {
+    localeMatcher: 'best fit',
+    numeric: 'always',
+    style: 'long',
+  });
+  const diff = Math.floor(
+    new Date(this.date_posted - new Date()) / (1000 * 60 * 60 * 24)
+  );
+  return rtf.format(diff, 'days');
+});
+
+module.exports = mongoose.model('Post', PostSchema);
---
+++ models/userSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+const Schema = mongoose.Schema;
+
+const UserSchema = newSchema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, default: false },
+  member: { type: Boolean, default: false },
+  join_date: { type: Date, default: Date.now },
+});
+
+UserSchema.virtual('fullName').get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+UserSchema.virtual('join_date_formatted').get(function () {
+  return DateTime.fromJSDate(this.join_date).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model('User', UserSchema);
