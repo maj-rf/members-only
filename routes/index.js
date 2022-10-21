141a84a2de9b81de013f27c8aaa064b3735f3fdd
bananabread08 <rmkfermin@gmail.com>
2022-10-21 10:05:21 +0800

    add GET method for user routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const userController = require('../controllers/userController');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Members Only' });
 });
+
+// User Routes
+router.get('/sign-up', userController.get_signup);
 
 module.exports = router;
