86ed4505ecd3425b5a7145e95ba23e043801d1b3
bananabread08 <rmkfermin@gmail.com>
2022-10-21 10:53:16 +0800

    creat postcss and tailwind configs
    add default tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.{html,js,pug}', './build/**/*.{html,js,pug}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  separator: '/',
+};
