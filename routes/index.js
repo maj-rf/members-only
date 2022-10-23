8f241edf01035ce55231020651ea0dbf1abffbde
bananabread08 <rmkfermin@gmail.com>
2022-10-23 11:06:28 +0800

    create POST router caller for sign-up and login

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,6 @@ router.get('/', function (req, res, next) {
 // User Routes
 router.get('/sign-up', userController.get_signup);
 router.get('/log-in', userController.get_login);
+router.post('/sign-up', userController.post_signup);
+router.post('/log-in', userController.post_login);
 module.exports = router;
