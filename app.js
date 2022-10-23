eca023a2884e332cbe4501da09b1f04fb41f2a52
bananabread08 <rmkfermin@gmail.com>
2022-10-23 11:07:03 +0800

    add authentication and serialization for users

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const bcrypt = require('bcryptjs');
+const User = require('./models/userSchema');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,10 +26,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Authentication
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) done(err);
+      if (!user) return done(null, false, { message: 'Incorrect username' });
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { msg: 'Incorrect password' });
+        }
+      });
+      return done(null, user);
+    });
+  }) // check if username exists and password matches
+);
+passport.serializeUser(function (user, done) {
+  done(null, user.id); // create a session cookie
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
 app.use(express.json());
