f1945f4f9001560b3f4dd8c44e78cbf1b5d31721
bananabread08 <rmkfermin@gmail.com>
2022-10-26 17:56:34 +0800

    add routes form messageForm
    fix index routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
+const indexController = require('../controllers/indexController');
+const postController = require('../controllers/postController');
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Members Only' });
-});
+router.get('/', indexController.get_index);
 
+// Posts Routes
+router.get('/messageForm', postController.message_get);
+router.post('/messageForm', postController.message_post);
 // User Routes
 router.get('/sign-up', userController.get_signup);
 router.get('/log-in', userController.get_login);
