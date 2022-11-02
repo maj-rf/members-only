133f8456b27b8e26870b45c9614ffd66caf9b3b1
bananabread08 <rmkfermin@gmail.com>
2022-11-02 11:12:06 +0800

    update with async

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,19 @@
 const Post = require('../models/postSchema');
 
-exports.get_index = (req, res, next) => {
-  Post.find({})
-    .sort({ date_posted: -1 })
-    .populate('user')
-    .populate('title')
-    .populate('message')
-    .populate('date_posted')
-    .exec(function (err, list) {
-      if (err) return next(err);
-      res.render('index', { title: 'Members Only', post_list: list });
+exports.get_index = async (req, res, next) => {
+  try {
+    const post_list = await Post.find({})
+      .sort({ date_posted: -1 })
+      .populate('user')
+      .populate('title')
+      .populate('message')
+      .populate('date_posted');
+    res.render('index', {
+      title: 'Members Only',
+      post_list: post_list,
+      user: req.user,
     });
+  } catch (err) {
+    return next(err);
+  }
 };
